6d0bb7b9b20fd609e5038ba6ca458f077e6142d8
Christoph Borowski <borowski.christoph@gmail.com>
2026-01-06 15:40:43 +0100

    Initial commit

---
+++ src/main/java/de/borowski/trainer/LearningTrainerApplication.java
@@ -0,0 +1,13 @@
+package de.borowski.trainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borowski/trainer/LearningTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borowski.trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
